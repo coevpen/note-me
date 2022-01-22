9830370ab5e0e008a84d956cca4f35b1d7a412b1
coevpen <koinuevins@gmail.com>
2022-01-21 22:51:05 -0600

    add unique id function and deletion function

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,14 +1,24 @@
 const router = require('express').Router();
 const fs = require('fs');
+
+// npm package to create a unique id
+const { nanoid } = require('nanoid');
+const id = nanoid();
+
+// sets up array for notes
 let notes;
 
+// gets the notes data
 router.get('/notes', (req, res) => {
     //reads the json file and returns to the client.
     notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
     res.json(notes);
 });
 
+// create new notes
 router.post('/notes', (req, res) => {
+    req.body.id = id;
+
     // adds the new note to the notes json
     let newNotes = req.body;
     notes.push(newNotes);
@@ -18,6 +28,20 @@ router.post('/notes', (req, res) => {
 
     // returns the new note to the client, posting it on the side bar
     res.json(notes);
+});
+
+// delete note upon clicking the trash can icon
+router.delete('/notes/:id', (req, res) => {
+    // get the note to delete by id
+    const noteId = req.params.id;
+    
+    let notesRemain = notes.filter( currNote => {
+        return currNote.id != noteId;
+    })
+
+    // writes the notes to the json file without the deleted note
+    fs.writeFileSync('db/db.json', JSON.stringify(notesRemain), 'utf8');
+    res.json(notesRemain);
 });
 
 module.exports = router;
